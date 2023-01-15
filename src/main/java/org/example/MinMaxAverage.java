c3737aca69f496dd6fe6bc53e16aa85cab434acb
Bush <adyurbash@mail.ru>
2023-01-15 22:06:12 +0300

    Bubble_sorting

--- src/main/java/org/example/MinMaxAverage.java
+++ src/main/java/org/example/MinMaxAverage.java
@@ -38,26 +38,4 @@ public class MinMaxAverage {
             average += (arr[i] / arr.length);
         }
         System.out.println("Average:  " + average);
-
-
-///////////////////SWAP/////////////////////////
-        int[] array = {10, 2, 10, 3, 1, 2, 5};
-        System.out.println(Arrays.toString(array));
-        boolean needIteration = true;
-        while (needIteration) {
-            needIteration = false;
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[i - 1]) {
-                    swap(array, i, i - 1);
-                    needIteration = true;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(array));
-    }
-    public static void swap(int[] arr1, int int1, int int2) {
-        int temp = arr1[int1];
-        arr1[int1] = arr1[int2];
-        arr1[int2] = temp;
-    }
-}
+}}
