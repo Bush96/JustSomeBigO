dbff17d93b55de2c70d99e1d6c8eb214b5792407
Bush <adyurbash@mail.ru>
2023-01-19 22:22:26 +0300

    Addings for delete things

--- src/main/java/org/example/MinMaxAverage.java
+++ src/main/java/org/example/MinMaxAverage.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class MinMaxAverage {
+
+
     public static void main(String[] args) {
+        int[] myArray = {1, 3, 5, 4, 3, 8, 9};        //for deleting;
+        removeElement(myArray, 3);
+
 
 
         int[] arr = new int[10];
@@ -40,4 +45,30 @@ public class MinMaxAverage {
             average += (arr[i] / arr.length);
         }
         System.out.println("Average:  " + average);
-}}
+
+
+
+    }
+
+    //////////////////DeletingElements///////////////////
+    public static int[] removeElement(int[] myArray, int num) {
+        int count = myArray.length;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == num) {
+                count--;                   //счетччик нужен чтобы узнать длину нвого массива
+            }
+
+        }
+        int[] newArray = Arrays.copyOf(myArray, count);
+        int offset = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == num) {
+                offset++;
+            } else {
+                newArray[i - offset] = myArray[i];            //тут прикольный прикол посмотри и вспомнишь
+            }
+        }
+        System.out.println(Arrays.toString(newArray));
+        return newArray;
+    }
+}
